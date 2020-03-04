341f318b0402223052953aa305db89a4f6eeed1d
Anas Merbouh <anas.merbouh@outlook.com>
2020-03-03 23:46:15 -0500

    add the ability to merge robot data sheets

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -1,6 +1,7 @@
 export let TEAMS = "teams";
 export let COMMENTS = "comments";
 export let TEAM_STATISTICS = "teamStatistics";
-export let MATCH_DATA_SHEETS = 'matchDataSheets';
+export let MATCH_DATA_SHEETS = "matchDataSheets";
+export let ROBOT_DATA_SHEETS = "robotDataSheets";
 export let POWER_CELL_STATISTICS = "powerCellStatistics";
 export let CLIMB_DURATION_STATISTICS = "climbDurationStatistics";
--- functions/src/rest-endpoints/index.ts
+++ functions/src/rest-endpoints/index.ts
@@ -1,18 +1,36 @@
 import { Request, Response } from "firebase-functions";
 import { firestoreInstance } from "..";
-import { MATCH_DATA_SHEETS } from "../constants";
+import { MATCH_DATA_SHEETS, ROBOT_DATA_SHEETS } from "../constants";
 
+/**
+ * Merges the data sheets sent by the client with the ones present on Cloud
+ * Firestore.
+ * 
+ * @param req A Request object representing the request sent by the client.
+ * @param res A Response object representing the response sent by the server
+ *            onces it finishes processing the request.
+ */
 export async function mergeDataSheets(req: Request, res: Response) {
     const batch = firestoreInstance.batch();
     const matchDataSheets: Array<any> = req.body.matchDataSheets;
-    
+    const robotDataSheets: Array<any> = req.body.robotDataSheets;
+
     // Get an instance of the match data sheets collection.
     const matchDataSheetsRef = firestoreInstance.collection(MATCH_DATA_SHEETS);
+
+    // Get an instance of the robot data sheets collection.
+    const robotDataSheetsRef = firestoreInstance.collection(ROBOT_DATA_SHEETS);
 
     // Create a document for each match data sheet, if applicable.
     matchDataSheets.forEach(sheet => {
         const matchDataSheetDocRef = matchDataSheetsRef.doc(sheet.id);
         batch.set(matchDataSheetDocRef, sheet, {merge:true});
+    });
+    
+    // Create a document for each robot sheet, if applicable.
+    robotDataSheets.forEach(sheet => {
+        const robotDataSheetDocRef = robotDataSheetsRef.doc(sheet.id);
+        batch.set(robotDataSheetDocRef, sheet, {merge:true});
     });
 
     // Commit the batch.
